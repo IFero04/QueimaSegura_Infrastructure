2d77c4dd15e7e0d7b0ffb6d6cd32ad1404bbd980
Afonso Faria <afonsofariatech@gmail.com>
2024-05-07 15:14:35 +0100

    Api Name

--- src/queimadas_api/main.py
+++ src/queimadas_api/main.py
@@ -1,15 +1,21 @@
 from typing import Union
 from fastapi import FastAPI
 from util.db import check_db
+from util.config import settings
 import time
 
+print("Loading DataBase ...")
+while not check_db(settings.pg_host, settings.pg_port, settings.pg_db_name, settings.pg_user, settings.pg_password):
+    print("Retrying in 15 seconds ...")
+    time.sleep(15)
+
+print("DataBase loaded.")
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
+    return {"Hello": settings.app_name}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
