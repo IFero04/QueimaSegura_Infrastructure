a9ed27022bfca8acf7f01d95c2e8ac87e10f776f
Afonso Faria <afonsofariatech@gmail.com>
2024-05-29 10:09:01 +0100

    Save

--- src/queimadas_api/api/location.py
+++ src/queimadas_api/api/location.py
@@ -4,6 +4,24 @@ from fastapi import HTTPException, status
 from util.db import PostgresDB
 from util.config import settings
 
+## QUERY
+def __build_query(row: str, number_results: int, limit: bool = True, isNumber: bool = False, perfect: bool = True):
+    base_query = f"""
+            SELECT *
+            FROM zip_codes
+    """
+    if isNumber:
+        where_query = f""" {base_query} WHERE {row} = %s """
+    else:
+        if perfect:
+            where_query = f""" {base_query} WHERE {row} LIKE %s """
+        else:
+            where_query = f""" {base_query} WHERE {row} ILIKE %s """
+    
+    if limit:
+        return f""" {where_query} LIMIT {number_results}; """
+    
+    return f""" {where_query}; """
 
 ## IDENTIFICATION
 def __calculate_probabilities(search_string: str):
@@ -68,17 +86,122 @@ def __calculate_probabilities(search_string: str):
         probabilities["location_name"] = 0.2
         return probabilities
 
+## FETCH DATA
+def __search_data(search_string: str,row:str, number_results: int, limit: bool = True, isNumber: bool = False, multi: bool = True):
+    with PostgresDB(settings.pg_host, settings.pg_port, settings.pg_db_name, settings.pg_user, settings.pg_password) as db:
+        query = __build_query(row, number_results, limit, isNumber, perfect=False)
+
+        parameters = (f"%{search_string}%", )
+        result = db.execute_query(query, parameters, multi=True)
+        result_json = []
+        for r in result:
+            result_json.append({
+                'id': r[0],
+                'location_code': r[1],
+                'location_name': r[2],
+                'ART_code': r[3],
+                'ART_name': r[4],
+                'tronco': r[5],
+                'client': r[6],
+                'zip_code': r[7],
+                'zip_name': r[8],
+                'county_id': r[9]
+            })
+        if result_json and not multi:
+            return result_json[0]
+        return result_json
+
+def __fetch_data(search_string: str, row: str, number_results: int, limit: bool = True, isNumber: bool = False, multi: bool = True):
+    with PostgresDB(settings.pg_host, settings.pg_port, settings.pg_db_name, settings.pg_user, settings.pg_password) as db:
+        query = __build_query(row, number_results, limit, isNumber, perfect=False)
+
+        parameters = (search_string, )
+        result = db.execute_query(query, parameters, multi=True)
+        result_json = []
+        for r in result:
+            result_json.append({
+                'id': r[0],
+                'location_code': r[1],
+                'location_name': r[2],
+                'ART_code': r[3],
+                'ART_name': r[4],
+                'tronco': r[5],
+                'client': r[6],
+                'zip_code': r[7],
+                'zip_name': r[8],
+                'county_id': r[9]
+            })
+        if result_json and not multi:
+            return result_json[0]
+        return result_json
+
 ## ITERATIONS
+def __find_location_name(tokens: list):
+    for token in tokens:
+        result = __fetch_data(token, 'location_name', 1, multi=False)
+        if result:
+            return token, result['location_code']
+    return None, None
+
+def __find_perfect_match(search_string: str, tokens: list, data: list):
+    keys = ['zip_code', 'location_name']
+    for token in tokens:
+        for item in data:
+            for key in keys:
+                if search_string.lower() in item[key].lower():
+                    return key, token
+    return None
 
 
-
-## ZIPCODE
+## LOCATION
 def get_location(search: str):
-    number_results = 50
+    tokens = re.split(r'[ ,\-]+', search)
+    locations = []
+    number_results = 100
+    perfect_match_key = None
+
+    location_name, location_code = __find_location_name(tokens)
+    if location_name:
+        tokens.remove(location_name)
+    
+    if location_code and len(tokens) == 0:
+        locations = __fetch_data(location_code, 'location_code', number_results, limit=False, isNumber=True)
+        if locations:
+            return {
+                'status': 'OK!',
+                'message': 'Location retrived successfully!',
+                'result': locations
+            }
+
     search_probabilities = __calculate_probabilities(search)
     order = [key for key in sorted(search_probabilities, key=search_probabilities.get, reverse=True) if search_probabilities[key] > 0.0]
+    
+    if search_probabilities['zip_code'] == 1:
+        locations = __fetch_data(search, 'zip_code', number_results, limit=False)
+        if locations:
+            return {
+                'status': 'OK!',
+                'message': 'Location retrived successfully!',
+                'result': locations
+            }
+
     for i in range(len(order)):
         print(f"Searching by {order[i]}: {search_probabilities[order[i]]}")
+        number_fetch = int(number_results * search_probabilities[order[i]])
+        try:
+            result = __fetch_data(search, order[i], number_fetch)
+            if result:
+                perfect_match_key = __find_perfect_match(search, tokens, result)
+                if perfect_match_key:
+                    break;
+                locations.extend(result)
+                
+        except Exception as e:
+            continue
+    
+    if perfect_match_key:
+        print(f"Perfect Match: {perfect_match_key}")
+        locations = __fetch_data(search, perfect_match_key, 0, limit=False)
 
     try:
         with PostgresDB(settings.pg_host, settings.pg_port, settings.pg_db_name, settings.pg_user, settings.pg_password) as db:
