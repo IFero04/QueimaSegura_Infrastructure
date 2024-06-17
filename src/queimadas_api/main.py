cf70c3375f8b210e9ac8ca17b8843227c008fd51
Afonso Faria <afonsofariatech@gmail.com>
2024-06-17 18:46:06 +0100

    Hot Fix

--- src/queimadas_api/main.py
+++ src/queimadas_api/main.py
@@ -28,7 +28,7 @@ class LoginCredentials(BaseModel):
 
 @app.get('/auth/check_email/', status_code=status.HTTP_200_OK, tags=['auth'])
 def check_email(email: str):
-    from api.users import valid_email
+    from api.auth import valid_email
     return valid_email(email)
 
 @app.get('/auth/check_session/', status_code=status.HTTP_200_OK, tags=['auth'])
@@ -38,12 +38,12 @@ def check_session(user_id: str, session_id: str):
 
 @app.post('/login/', status_code=status.HTTP_202_ACCEPTED ,tags=['auth'])
 def login(credentials: LoginCredentials):
-    from api.users import login
+    from api.auth import login
     return login(credentials)
 
 @app.delete('/logout/', status_code=status.HTTP_202_ACCEPTED, tags=['auth'])
 def logout(user_id: str, session_id: str):
-    from api.users import logout
+    from api.auth import logout
     return logout(user_id, session_id)
 
 
