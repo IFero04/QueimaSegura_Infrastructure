b18854e76574979e5f0726cd56ff1b6990b6eee8
Afonso Faria <afonsofariatech@gmail.com>
2024-06-17 18:55:37 +0100

    Hot Fixes

--- src/queimadas_api/api/auth.py
+++ src/queimadas_api/api/auth.py
@@ -30,25 +30,29 @@ def valid_email(email):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
 def check_session(user_id, session_id):
-    with PostgresDB(settings.pg_host, settings.pg_port, settings.pg_db_name, settings.pg_user, settings.pg_password) as db:
-        query = """
-            SELECT session_id
-            FROM users
-            WHERE id = %s
-        """
-        parameters = (user_id, )
-        result = db.execute_query(query, parameters, multi=False)
-        if not result:
-            raise Exception('User not found')
-        
-        if active_session := result[0]:
-            if active_session != session_id:
-                raise Exception('Session_id does not match')
+    try:
+        with PostgresDB(settings.pg_host, settings.pg_port, settings.pg_db_name, settings.pg_user, settings.pg_password) as db:
+            query = """
+                SELECT session_id
+                FROM users
+                WHERE id = %s
+            """
+            parameters = (user_id, )
+            try:
+                result = db.execute_query(query, parameters, multi=False)
+            except Exception as e:
+                raise Exception('User not found')
+            
+            if active_session := result[0]:
+                if active_session != session_id:
+                    raise Exception('Session_id does not match')
 
-        return {
-            'status': 'OK!',
-            'message': 'Session is valid!'
-        }
+            return {
+                'status': 'OK!',
+                'message': 'Session is valid!'
+            }
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
 
 ## LOGIN
 def _check_login(credentials):
