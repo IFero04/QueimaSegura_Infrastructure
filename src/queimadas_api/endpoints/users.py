e2220458908182719d0c9e9c7d6edd7d3b199ce5
Afonso Faria <afonsofaraitech@gmail.com>
2024-06-18 20:20:51 +0100

    Fix

--- src/queimadas_api/endpoints/users.py
+++ src/queimadas_api/endpoints/users.py
@@ -13,9 +13,13 @@ class User(BaseModel):
 @router.post('/create/', status_code=status.HTTP_201_CREATED)
 def create_user(user: User):
     from api.users import create_user
+    user.email = user.email.lower()
+    user.fullName = user.fullName.title()
     return create_user(user)
 
 @router.put('/update/{user_id}/{session_id}/', status_code=status.HTTP_202_ACCEPTED)
 def update_user(user_id: str, session_id: str, user: User):
     from api.users import update_user
+    user.email = user.email.lower()
+    user.fullName = user.fullName.title()
     return update_user(user_id, session_id, user)
