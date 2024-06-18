93173915428540105535ebffe1a6fa0bd14425b8
Afonso Faria <afonsofaraitech@gmail.com>
2024-06-18 20:19:03 +0100

    title

--- src/queimadas_api/endpoints/admin.py
+++ src/queimadas_api/endpoints/admin.py
@@ -14,4 +14,5 @@ class User(BaseModel):
 def create_user(admin_id: str, session_id: str , user: User):
     from api.admin import create_user
     user.email = user.email.lower()
+    user.fullName = user.fullName.title()
     return create_user(admin_id, session_id, user)
