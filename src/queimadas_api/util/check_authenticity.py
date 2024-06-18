6153a7975851a970668880dd494a7a7607e53260
Afonso Faria <afonsofariatech@gmail.com>
2024-06-18 17:03:35 +0100

    Teste

--- src/queimadas_api/util/check_authenticity.py
+++ src/queimadas_api/util/check_authenticity.py
@@ -15,6 +15,8 @@ def check_session(user_id, session_id):
             raise ('User not found')
         
         if active_session := result[0]:
+            if active_session == None:
+                raise Exception('Session does not exist')
             if active_session != session_id:
                 raise Exception('Session does not match')
 
@@ -32,6 +34,8 @@ def check_admin_authenticity(admin_id, session_id):
             raise ('User not found')
         
         if active_session := result[0]:
+            if active_session == None:
+                raise Exception('Session does not exist')
             if active_session != session_id:
                 raise Exception('Session does not match')
         if type := result[1]:
