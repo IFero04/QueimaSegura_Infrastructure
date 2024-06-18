435a3950dc06e0cf4660bbac80c755946c6cf466
Afonso Faria <afonsofariatech@gmail.com>
2024-06-18 16:26:53 +0100

    Teste

--- src/queimadas_api/endpoints/auth.py
+++ src/queimadas_api/endpoints/auth.py
@@ -6,24 +6,24 @@ router = APIRouter()
 class LoginCredentials(BaseModel):
     email: str
     password: str
-    
 
-@router.get('/auth/check_email/', status_code=status.HTTP_200_OK)
+
+@router.get('/check_email/', status_code=status.HTTP_200_OK)
 def check_email(email: str):
     from api.auth import valid_email
     return valid_email(email)
 
-@router.get('/auth/check_session/', status_code=status.HTTP_200_OK, tags=['auth'])
+@router.get('/check_session/', status_code=status.HTTP_200_OK)
 def check_session(user_id: str, session_id: str):
     from api.auth import check_session
     return check_session(user_id, session_id)
 
-@router.post('/login/', status_code=status.HTTP_202_ACCEPTED ,tags=['auth'])
+@router.post('/login/', status_code=status.HTTP_202_ACCEPTED)
 def login(credentials: LoginCredentials):
     from api.auth import login
     return login(credentials)
 
-@router.delete('/logout/', status_code=status.HTTP_202_ACCEPTED, tags=['auth'])
+@router.delete('/logout/', status_code=status.HTTP_202_ACCEPTED)
 def logout(user_id: str, session_id: str):
     from api.auth import logout
     return logout(user_id, session_id)
