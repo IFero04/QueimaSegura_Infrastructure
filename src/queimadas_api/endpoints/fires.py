a3463bff391bd03f40d2d2b328dbcfb5cab11f72
Afonso Faria <afonsofaraitech@gmail.com>
2024-06-21 22:25:35 +0100

    Update

--- src/queimadas_api/endpoints/fires.py
+++ src/queimadas_api/endpoints/fires.py
@@ -13,12 +13,12 @@ class Fire(BaseModel):
     observations: Union[str, None] = None
 
 
-@router.post('/create/{user_id}/{session_id}/', status_code=status.HTTP_201_CREATED)
+@router.post('/create/{user_id}', status_code=status.HTTP_201_CREATED)
 def new_fire(user_id: str, session_id: str, fire: Fire):
     from api.fires import create_fire
     return create_fire(user_id, session_id, fire)
 
-@router.get('/user/{user_id}/{session_id}/', status_code=status.HTTP_200_OK)
+@router.get('/user/{user_id}', status_code=status.HTTP_200_OK)
 def get_fires_by_user(user_id: str, session_id: str):
     from api.fires import get_user_fires
     return get_user_fires(user_id, session_id)
