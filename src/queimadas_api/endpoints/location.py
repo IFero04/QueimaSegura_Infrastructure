9e2d570bbb8d7d358f221f9766a8194fe245f2d2
Afonso Faria <afonsofaraitech@gmail.com>
2024-06-23 15:45:00 +0100

    Fix

--- src/queimadas_api/endpoints/location.py
+++ src/queimadas_api/endpoints/location.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, status
 router = APIRouter()
 
 
-@router.get('/', status_code=status.HTTP_202_ACCEPTED)
+@router.get('', status_code=status.HTTP_202_ACCEPTED)
 def get_location(search: str):
     from api.location import handle_location_response
     return handle_location_response(search)
