80a8a46c3a5d1f1179ef46dcc675e9895bbfdb7b
Afonso Faria <afonsofariatech@gmail.com>
2024-06-27 14:11:53 +0100

    Print

--- src/queimadas_api/api/admin.py
+++ src/queimadas_api/api/admin.py
@@ -37,6 +37,7 @@ def get_admin_status(admin_id, session_id):
             parameters = (admin_id, )
             result = db.execute_query(query, parameters)
             fires_approved = result
+            print(fires_approved, fires_to_aprrove)
 
         return {
             'status': 'OK!',
