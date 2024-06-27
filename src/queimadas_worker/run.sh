96e6aa7bb01f0fb23469aa876675ab22d4089634
Afonso Faria <afonsofariatech@gmail.com>
2024-06-27 10:05:35 +0100

    Update

--- src/queimadas_worker/run.sh
+++ src/queimadas_worker/run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 while true; do
-  if [ "$USE_DEV_MODE" = "true" ];
-    then nodemon --legacy-watch --exec python -u main.py $PG_HOST $PG_PORT $PG_DB_NAME $PG_ADMIN_USER $PG_ADMIN_PASSWORD; 
-    else python -u main.py $PG_HOST $PG_PORT $PG_DB_NAME $PG_ADMIN_USER $PG_ADMIN_PASSWORD;
-  fi
+  python -u main.py $PG_HOST $PG_PORT $PG_DB_NAME $PG_ADMIN_USER $PG_ADMIN_PASSWORD;
 
   echo "Executed worker at $(date)"
   echo "Sleeping for 3 hours"
