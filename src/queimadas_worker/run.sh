6488534c50ada9ffab299c2d20c075cd43eb4b98
Afonso Faria <afonsofariatech@gmail.com>
2024-06-27 10:02:26 +0100

    Update

--- src/queimadas_worker/run.sh
+++ src/queimadas_worker/run.sh
@@ -6,7 +6,7 @@ while true; do
     else python -u main.py $PG_HOST $PG_PORT $PG_DB_NAME $PG_ADMIN_USER $PG_ADMIN_PASSWORD;
   fi
 
-  echo "Executed scraper at $(date)"
+  echo "Executed worker at $(date)"
   echo "Sleeping for 3 hours"
   sleep 10800
 done
