53c5f9c799dc9601dfc9b141f2577e4e5266111e
Afonso Faria <afonsofariatech@gmail.com>
2024-06-28 09:24:45 +0100

    Teste

--- src/queimadas_worker/main.py
+++ src/queimadas_worker/main.py
@@ -6,7 +6,7 @@ def main():
     with PostgresDB(PG_HOST, PG_PORT, PG_DB_NAME, PG_USER, PG_PASSWORD) as db:
         query = """
             UPDATE public.fires
-            SET status = DEFAULT
+            SET date = date
         """
         db.execute_query(query, fetch=False)
 
